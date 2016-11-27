239cde052ebf0d46ba705b666bab5bed6d9447db
Armand-4A6A <517226@edu.rocmn.nl>
2016-11-27 14:13:04 +0100

    added test information return to display, blocked overwriting a filled block,begon constructing cpu opponent.

--- butter_cheese_and_eggs_javascript.js
+++ butter_cheese_and_eggs_javascript.js
@@ -1,22 +1,48 @@
-var numbarray = ['','one','two','three','four','five','six','seven','eight','nine']
-var nmb = 0
-var blok = "block_"
-var blcknumb = ""
-var turnswitch = 1
-var varturncount = 0
-var varprint = ""
+//saved arrayof strings
+var numbarray = ['','one','two','three','four','five', 'six','seven','eight','nine']
+
+// used to store the information of each block in the 9grid
 var arrayofmoves = []
-var character_O = "O"
-var character_X = "X"
-var character = ""
+var nmb = 0
+
+//used to store temporarly information
+var blcknumb = "" //used to store an Id number
+var varprint = "" //used to store a message before returning it to UI
+var character = "" // used to save a character after each turn
+
+//Counter for the amount of passed turns
+var varturncount = 0
+
+// saved strings
+var blok = "block_"
 var P_one = "player 1"
+var character_O = "O" // 0 is for player1
 var P_two = "player 2"
-var player = 1
-var cpu = 0
-var game = 0
+var character_X = "X" // X is for player2
+
+var player = 1 //used to indentify player 1
+
+//switches
+var turnswitch = 1 	//1 = player1   			2 = player2 or cpu
+var cpu = 0 		//1 = singleplayer   		0 = multiplayer
+var game = 0 		//0 = game in process   	1 = game ended
 
 /////////////////////////////////////////////////////////////////////
+function setpmode_one(){
+	cpu = 1
+	reset()
+	varprint = "Computerplayer is on"
+	printdisplay()
+	turnswitch = 1
 	
+}
+function setpmode_two(){
+	cpu = 0
+	reset()
+}
+
+///////////////////////////////////////////////////////////////////
+
 //Reset function
 function reset(){
 	for(i=1; i<=9; i++){
@@ -28,7 +54,7 @@ function reset(){
 	turnswitcher()
 	turnswitcher()
 	printdisplay()
-	arrayofmoves = ['A','B','C','D','E','F','G','H','I','J']
+	arrayofmoves = ['','B','C','D','E','F','G','H','I','J']
 	game = 0
 }
 /////////////////////////////////////////////////////////////////////
@@ -36,14 +62,18 @@ function reset(){
 //Core of the program
 function execute(){
 	if(game == 0){
-	blcknumb = blok+numbarray[nmb];	//saves ID name
-	turnswitcher();					//Switches player after each turn
-	printdisplay();					//Return's to display
-	turncounter();					//counts turn's
-	arrayofmoves[nmb] = turnswitch; //Saves player action into array
-	document.getElementById(blcknumb).innerHTML = character; //writes O or X to ID
-	win(); 							//Tests for a won game
-	cpu_opponent();					//Plays computer opponent
+		if(arrayofmoves[nmb] != 1 && arrayofmoves[nmb] != 2){
+			blcknumb = blok+numbarray[nmb];	//saves ID name
+			turnswitcher();					//Switches player after each turn
+			printdisplay();					//Return's to display
+			turncounter();					//counts turn's
+			arrayofmoves[nmb] = turnswitch; //Saves player action into array
+			document.getElementById(blcknumb).innerHTML = character; //writes O or X to ID
+			win(); 							//Tests for a won game
+			cpu_opponent();					//Plays computer opponent
+			varprint +="<br />"+arrayofmoves
+			printdisplay()
+		}
 	}
 }
 ////////////////////////////////////////////////////////////////////
@@ -127,6 +157,78 @@ function winprint(){
 }
 
 ////////////////////////////////////////////////////////////////
+
+//Computer opponent{
+function cpu_opponent(){
+	if(cpu == 1 && turnswitch == 2){
+		//Captures the middle
+		if(arrayofmoves[5] == "F" && arrayofmoves != 1 ||arrayofmoves != 2){
+			block__five()
+		}
+		
+		//makes sure top horizontal row isn't captured
+		else if(arrayofmoves[1] != 2 && arrayofmoves[2] == 1 && arrayofmoves[2] == 1){
+			block__one()
+		}
+		else if(arrayofmoves[1] == 1 && arrayofmoves[2] != 2 && arrayofmoves[3] == 1){
+		block__two()
+		}
+		else if(arrayofmoves[1] == 1 && arrayofmoves[2] != 2 && arrayofmoves[3] != 2){
+			block__three()
+		}
+		
+		//makes sure middle horizontal row isn't captured
+		else if(arrayofmoves[4] != 2 && arrayofmoves[5] == 1 && arrayofmoves[6] == 1){
+			block__four()
+		}
+		else if(arrayofmoves[4] == 1 && arrayofmoves[5] != 2 && arrayofmoves[6] == 1){
+			block__five()
+		}
+		else if(arrayofmoves[4] == 1 && arrayofmoves[5] != 2 && arrayofmoves[6] != 2){
+			block__six()
+		}
+		
+		//makes sure bottom horizontal row isn't captured
+		else if(arrayofmoves[7] != 2 && arrayofmoves[8] == 1 && arrayofmoves[9] == 1){
+			block__seven()
+		}
+		else if(arrayofmoves[7] == 1 && arrayofmoves[8] != 2 && arrayofmoves[9] == 1){
+			block__eight()
+		}
+		else if(arrayofmoves[7] == 1 && arrayofmoves[8] != 2 && arrayofmoves[9] != 2){
+			block__nine()
+		}
+		///////////////////////////////////////
+		// makes sure left verticalrow isn't captured
+		else if(arrayofmoves[1] != 2 && arrayofmoves[4] == 1 && arrayofmoves[7] == 1){
+			block__one()
+		}
+		else if(arrayofmoves[2] != 2 && arrayofmoves[5] == 1 && arrayofmoves[8] == 1){
+			block__two()
+		}
+		else if(arrayofmoves[3] != 2 && arrayofmoves[6] == 1 && arrayofmoves[6] == 1){
+			block__three()
+		}
+		
+		
+		
+				
+		else if(arrayofmoves[3] == "X"){
+			block__seven()
+		}
+		else if(arrayofmoves[7] == "X"){
+			block__three()
+		}
+		else if(arrayofmoves[9] == "X"){
+			block__one()
+		}
+		//test info
+		varprint +="<br />"+ nmb
+		printdisplay()
+	}
+}
+
+/////////////////////////////////////////////////////////////////
 
 //Blocks to number input
 function block__one(){
