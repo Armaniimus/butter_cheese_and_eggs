9b6d576ec49b460732d27521ae2a6d8318a87ac9
Armand-4A6A <517226@edu.rocmn.nl>
2016-11-27 04:40:57 +0100

    added a display, a print to display function, 3 buttons, and a reset function.

--- butter_cheese_and_eggs_javascript.js
+++ butter_cheese_and_eggs_javascript.js
@@ -1,14 +1,32 @@
 var numbarray = ['','one','two','three','four','five','six','seven','eight','nine']
-var nmb = ''
+var nmb = 0
 var blok = "block_"
 var blcknumb = ""
-var turnswitch = "X"
-var turncount = 0
+var turnswitch = "O"
+var varturncount = 0
+var varprint = ""
+var arrayblocktypes = []
 
-function print(){
+//Reset function
+function reset(){
+	for(i=1; i<=9; i++){
+		arrayblocktypes[i] = "";
+		blcknumb = blok+numbarray[i];
+		document.getElementById(blcknumb).innerHTML = '';
+	}
+	varprint = "Its player 1 its turn"
+	printdisplay()
+	varturncount = 0
+	turnswitch = "O"
+}
+//Print to display
+function printdisplay(){
+	document.getElementById("display").innerHTML = varprint
+}
+//Core of the program
+function execute(){
 	blcknumb = blok+numbarray[nmb]
 	document.getElementById(blcknumb).innerHTML = turnswitch
-	
 	turnswitcher()
 	turncounter()
 }
@@ -16,55 +34,59 @@ function print(){
 function turnswitcher(){
 	if (turnswitch == "X"){
 		turnswitch = "O"
+		varprint = "Its player 1 it's turn"
+		printdisplay()
 	}
 	else{
 		turnswitch = "X"
+		varprint = "Its player 2 it's turn"
+		printdisplay()
 	}
 }
 //Counts the turns
 function turncounter(){
-	turncount += 1
-	if(turncount == 9){
-		turncount = 0
-		alert("Draw")
+	varturncount += 1
+	if(varturncount == 9){
+		varturncount = 0
+		varprint = "The game is a draw"
+		printdisplay()
 	}
-	
 }
-
-
+//Blocks to number input
 function block__one(){
 	nmb = 1
-	print()
+	execute()
 }
 function block__two(){
 	nmb = 2
-	print()
+	execute()
 }
 function block__three(){
 	nmb = 3
-	print()
+	execute()
 }
 function block__four(){
 	nmb = 4
-	print()
+	execute()
 }
 function block__five(){
 	nmb = 5
-	print()
+	execute()
 }
 function block__six(){
 	nmb = 6
-	print()
+	execute()
 }
 function block__seven(){
 	nmb = 7
-	print()
+	execute()
 }
 function block__eight(){
 	nmb = 8
-	print()
+	execute()
 }
 function block__nine(){
 	nmb = 9
-	print()
+	execute()
 }
+reset()
